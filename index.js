e68e872937e87b1c32e388ef84001ff457a1df5f
Ian Dilling <ianbjorndilling@gmail.com>
2016-02-20 15:59:22 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+function lines(str) {
+  return str.trim().split(/\n/);
+}
+
+function strip_comment(str) {
+  return str.trim().split('#')[0];
+}
+
+function str_to_vec(str) {
+  return str.trim().split(/\s/).map(Number);
+}
+
+function is_empty(str) {
+  return str.length == 0;
+}
+
+str_to_face(str) {
+  return str_to_vec(str).slice(1);
+}
+
+function off2json(str) {
+
+  var offdata = lines(str.trim())
+    .map(strip_comment)
+    .filter(is_empty);
+
+  if (offdata[0] != 'OFF') {
+    throw new Error("File contents not correctly formatted");
+  }
+
+  var counts = str_to_vec(offdata[1]);
+  var vertex_count = counts[0];
+
+  var vertices = offdata.slice(2, vertex_count + 2).map(str_to_vec);
+  var faces = offdata.slice(vertex_count + 2).map(str_to_face);
+
+  return {
+    vertices: vertices,
+    faces: faces;
+  };
+
+}
+
+module.exports = off2json;
+
